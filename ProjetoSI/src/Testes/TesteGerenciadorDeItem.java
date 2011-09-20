291dfcf5e5324aca8c4ae0f440d1b50b8550bad5
rodolfodelimaviana@gmail.com <rodolfodelimaviana@gmail.com@f405434d-50e6-5b59-0e69-a16f1f18b76f>
2011-09-20 23:02:55 +0000

    Mais alguns testes. Sem erro

---
+++ ProjetoSI/src/Testes/TesteGerenciadorDeItem.java
@@ -0,0 +1,17 @@
+package Testes;
+
+import junit.framework.Assert;
+import junit.framework.AssertionFailedError;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Classes.GerenciadorAmizades;
+import Classes.Usuario;
+
+public class TesteGerenciadorDeItem {
+	
+	
+	
+	
+}
